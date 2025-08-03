ee510c14b1d31a83bebfcf512188bd94beadfdda
zima563 <mohamedabdelazim654@gmail.com>
2025-08-03 12:43:49 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,8 @@ async function bootstrap() {
     origin: [
       'http://localhost:5174',
       'http://127.0.0.1:5174',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
       'http://localhost:3000',
       'http://127.0.0.1:3000',
       'https://your-frontend-domain.com', // ضيف هنا الدومين الفعلي لو عندك
