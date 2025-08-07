81f2ccaa35c196f04ab4e252e71aebf912c44d97
mohamedAbdelazim <mohamedabdelazim654@gmail.com>
2025-08-07 14:04:03 +0300

    UPDATE name of models

--- prisma/migrations/20250807104302_update_maxlength_of_description/migration.sql
+++ prisma/migrations/20250807104302_update_maxlength_of_description/migration.sql
@@ -9,7 +9,7 @@ ALTER TABLE `Certificate` MODIFY `descriptionEn` VARCHAR(1000) NULL,
     MODIFY `descriptionFr` VARCHAR(1000) NULL;
 
 -- AlterTable
-ALTER TABLE `Contactus` MODIFY `subject` VARCHAR(2000) NOT NULL;
+ALTER TABLE `ContactUs` MODIFY `subject` VARCHAR(2000) NOT NULL;
 
 -- AlterTable
 ALTER TABLE `Industry` MODIFY `descriptionEn` VARCHAR(1000) NULL,
