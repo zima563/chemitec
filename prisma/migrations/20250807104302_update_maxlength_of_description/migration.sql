2088b05dfceac373490f0b230ca26fc445ea5101
mohamedAbdelazim <mohamedabdelazim654@gmail.com>
2025-08-07 14:00:48 +0300

    UPDATE name of models

--- prisma/migrations/20250807104302_update_maxlength_of_description/migration.sql
+++ prisma/migrations/20250807104302_update_maxlength_of_description/migration.sql
@@ -1,32 +1,32 @@
 -- AlterTable
-ALTER TABLE `brand` MODIFY `descriptionEn` VARCHAR(1000) NULL,
+ALTER TABLE `Brand` MODIFY `descriptionEn` VARCHAR(1000) NULL,
     MODIFY `descriptionAr` VARCHAR(1000) NULL,
     MODIFY `descriptionFr` VARCHAR(1000) NULL;
 
 -- AlterTable
-ALTER TABLE `certificate` MODIFY `descriptionEn` VARCHAR(1000) NULL,
+ALTER TABLE `Certificate` MODIFY `descriptionEn` VARCHAR(1000) NULL,
     MODIFY `descriptionAr` VARCHAR(1000) NULL,
     MODIFY `descriptionFr` VARCHAR(1000) NULL;
 
 -- AlterTable
-ALTER TABLE `contactus` MODIFY `subject` VARCHAR(2000) NOT NULL;
+ALTER TABLE `Contactus` MODIFY `subject` VARCHAR(2000) NOT NULL;
 
 -- AlterTable
-ALTER TABLE `industry` MODIFY `descriptionEn` VARCHAR(1000) NULL,
+ALTER TABLE `Industry` MODIFY `descriptionEn` VARCHAR(1000) NULL,
     MODIFY `descriptionAr` VARCHAR(1000) NULL,
     MODIFY `descriptionFr` VARCHAR(1000) NULL;
 
 -- AlterTable
-ALTER TABLE `product` MODIFY `descriptionEn` VARCHAR(1000) NULL,
+ALTER TABLE `Product` MODIFY `descriptionEn` VARCHAR(1000) NULL,
     MODIFY `descriptionAr` VARCHAR(1000) NULL,
     MODIFY `descriptionFr` VARCHAR(1000) NULL;
 
 -- AlterTable
-ALTER TABLE `successfulproduct` MODIFY `descriptionEn` VARCHAR(1000) NULL,
+ALTER TABLE `Successfulproduct` MODIFY `descriptionEn` VARCHAR(1000) NULL,
     MODIFY `descriptionAr` VARCHAR(1000) NULL,
     MODIFY `descriptionFr` VARCHAR(1000) NULL;
 
 -- AlterTable
-ALTER TABLE `teammember` MODIFY `bioEn` VARCHAR(200) NULL,
+ALTER TABLE `Teammember` MODIFY `bioEn` VARCHAR(200) NULL,
     MODIFY `bioAr` VARCHAR(200) NULL,
     MODIFY `bioFr` VARCHAR(200) NULL;
