06654c2fd02dce1e46dd19e88532276de438735f
mohamedAbdelazim <mohamedabdelazim654@gmail.com>
2025-08-30 17:07:25 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -42,6 +42,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+await app.listen(port, '0.0.0.0');
 }
 bootstrap();
